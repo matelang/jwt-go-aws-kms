d9cf2cbc42785c262a83f7f73109ceb2f0c4f5f4
Garret Kelly <garret.kelly@robinhood.com>
2022-10-03 02:13:07 -0400

    jwtkms: Add a KMS partial mock and jwtkms tests (#9)
    This commit adds a new `internal/mockkms` package which provides an
    implementation of the KMSClient interface backed by in-memory storage.
    The mock isn't as strict as the real KMS, but does implement the Sign,
    Verify, and GetPublicKey methods faithfully enough to allow for testing
    of jwtkms.
    
    This commit also introduces tests for all of the jwtkms signing methods,
    using the mockkms package to provide the backing implementation.

---
+++ jwtkms/signingmethod_test.go
@@ -0,0 +1,81 @@
+package jwtkms
+
+import (
+	"testing"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/matelang/jwt-go-aws-kms/v2/jwtkms/internal/mockkms"
+)
+
+func TestSigningMethod(t *testing.T) {
+	tests := []struct {
+		name          string
+		keyType       mockkms.KeyType
+		signingMethod jwt.SigningMethod
+	}{
+		{
+			name:          "ES256",
+			keyType:       mockkms.KeyTypeECCNISTP256,
+			signingMethod: SigningMethodECDSA256,
+		},
+		{
+			name:          "ES384",
+			keyType:       mockkms.KeyTypeECCNISTP384,
+			signingMethod: SigningMethodECDSA384,
+		},
+		{
+			name:          "ES512",
+			keyType:       mockkms.KeyTypeECCNISTP521,
+			signingMethod: SigningMethodECDSA512,
+		},
+		{
+			name:          "RS256",
+			keyType:       mockkms.KeyTypeRSA2048,
+			signingMethod: SigningMethodRS256,
+		},
+		{
+			name:          "RS384",
+			keyType:       mockkms.KeyTypeRSA2048,
+			signingMethod: SigningMethodRS384,
+		},
+		{
+			name:          "RS512",
+			keyType:       mockkms.KeyTypeRSA2048,
+			signingMethod: SigningMethodRS512,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			token := jwt.NewWithClaims(test.signingMethod, &jwt.MapClaims{
+				"claim": "value",
+			})
+
+			kms := mockkms.NewMockKMS()
+			id, err := kms.GenerateKey(test.keyType)
+			if err != nil {
+				t.Fatalf("Error generating key: %v", err)
+			}
+
+			config := NewKMSConfig(kms, id, false)
+			signed, err := token.SignedString(config)
+			if err != nil {
+				t.Fatalf("Error signing token: %v", err)
+			}
+
+			var claims jwt.MapClaims
+			_, err = jwt.ParseWithClaims(signed, &claims, func(*jwt.Token) (interface{}, error) {
+				return NewKMSConfig(kms, id, false), nil
+			})
+			if err != nil {
+				t.Fatalf("Error validating token offline: %v", err)
+			}
+
+			_, err = jwt.ParseWithClaims(signed, &claims, func(*jwt.Token) (interface{}, error) {
+				return NewKMSConfig(kms, id, true), nil
+			})
+			if err != nil {
+				t.Fatalf("Error validating token online: %v", err)
+			}
+		})
+	}
+}
